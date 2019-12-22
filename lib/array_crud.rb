a870649418f2f561bfe4e1011ef283f95b1bbf6c
Muhammad Afif Alaptagin <afiffifa89@gmail.com>
2019-12-22 06:40:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ def create_an_array
   
 end
 
-def add_element_to_end_of_array(cars, names)
+def add_element_to_end_of_array(array, element)
   cars = ["Ford", "Toyota", "Honda"]
   cars << "Mazda"
   
